0e7e2981b196ec5289c5658ff317661d2a8466f7
Robert <rha006@gmail.com>
2024-03-02 23:09:37 +0100

    Fixing url trailing slash error

--- shortener/urls.py
+++ shortener/urls.py
@@ -4,9 +4,9 @@ from shortener import views
 
 urlpatterns = [
     path("", views.shortener, name="shortener"),
-    path('u/<str:short_url_extension>/', views.redirect_url, name='redirect_url'),
+    path('u/<str:short_url_extension>', views.redirect_url, name='redirect_url'),
     path('track/', views.tracking, name='track'),
-    path('t/<str:short_url_extension>/', views.track_url, name='track_url'),
+    path('t/<str:short_url_extension>', views.track_url, name='track_url'),
     path('report/', views.report_url, name='report'),
-    path('w/<str:short_url_extension>/', views.rediect_warning, name='warning'),
+    path('w/<str:short_url_extension>', views.rediect_warning, name='warning'),
 ]
