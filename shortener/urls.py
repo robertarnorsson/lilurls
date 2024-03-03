9df042924acec0313bb1cff714784dc154e06f33
Robert <rha006@gmail.com>
2024-03-03 11:13:28 +0100

    Typo error

--- shortener/urls.py
+++ shortener/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('t/<str:short_url_extension>', views.track_url, name='track_url'),
     path('t/<str:short_url_extension>/', views.track_url, name='track_url_slash'),
     path('report/', views.report_url, name='report'),
-    path('w/<str:short_url_extension>', views.rediect_warning, name='warning'),
-    path('w/<str:short_url_extension>/', views.rediect_warning, name='warning_slash'),
+    path('w/<str:short_url_extension>', views.redirect_warning, name='warning'),
+    path('w/<str:short_url_extension>/', views.redirect_warning, name='warning_slash'),
 ]
