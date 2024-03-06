da2279181f482faec0f50ec6e68d225db1e9964e
Robert <rha006@gmail.com>
2024-03-06 20:46:34 +0100

    Added reCaptcha to tracking and report

--- shortener/forms.py
+++ shortener/forms.py
@@ -10,24 +10,27 @@ class UrlForm(forms.Form):
         label="Website URL",
         widget=forms.URLInput(attrs={'class': 'w-full py-3 px-4 text-gray-300 placeholder-dark-600 bg-dark-900 rounded-l border-2 border-transparent focus:outline-none focus:border-neutral-800'})
     )
-    captcha_field = ReCaptchaField(
-        widget=ReCaptchaV3(
-            action='shorten'
-        )
-    )
+    captcha = ReCaptchaField(widget=ReCaptchaV3(action='shorten'))
 
 class TrackingForm(forms.Form):
     tracking_url = forms.URLField(
         label="Shortened URL",
         widget=forms.URLInput(attrs={'class': 'w-full py-3 px-4 text-gray-300 placeholder-dark-600 bg-dark-900 rounded-l border-2 border-transparent focus:outline-none focus:border-neutral-800'})
     )
+    captcha = ReCaptchaField(widget=ReCaptchaV3(action='tracking'))
 
 class ReportForm(forms.ModelForm):
+    captcha = ReCaptchaField(widget=ReCaptchaV3(action='report'))
+
     class Meta:
         model = models.Report
-        fields = ['url', 'violation_type', 'message']
+        fields = ['url', 'violation_type', 'message', 'captcha']
         widgets = {
             'url': forms.URLInput(attrs={'placeholder': 'Shortened URL','class': 'w-full py-3 px-4 text-gray-300 placeholder-dark-600 bg-dark-900 rounded border-2 border-transparent focus:outline-none focus:border-neutral-800'}),
             'violation_type': forms.Select(attrs={'class': 'w-full py-3 px-4 text-gray-300 placeholder-dark-600 bg-dark-900 rounded border-2 border-transparent focus:outline-none focus:border-neutral-800'}),
             'message': forms.Textarea(attrs={'placeholder': 'Message','class': 'w-full py-3 px-4 text-gray-300 placeholder-dark-600 bg-dark-900 rounded border-2 border-transparent focus:outline-none focus:border-neutral-800', 'rows': 4}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(ReportForm, self).__init__(*args, **kwargs)
+        self.fields['captcha'].required = True
