57ba6258a9c70379a7ee9c844f3ade08d8c57812
Robert <rha006@gmail.com>
2024-03-06 18:12:21 +0100

    Added ads.txt to urls

--- lilurls/urls.py
+++ lilurls/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("terms/", TemplateView.as_view(template_name="terms.html"), name="terms"),
     path("privacy/", TemplateView.as_view(template_name="privacy.html"), name="privacy"),
     path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots"),
+    path("ads.txt",TemplateView.as_view(template_name="ads.txt", content_type="text/plain"), name="ads"),
     path('admin/', admin.site.urls),
     path('captcha/', include('captcha.urls'))
 ]
